4fe3838c28ec3661a48c3136da2ae6d685e08541
unknown <camposmarbella898029@ymail.com>
2021-02-05 15:39:15 -0600

    initial commit

---
+++ Binary Search Tree/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search_Tree
+{
+    class Node
+    {
+        //Member Variables
+
+        //Constructor
+
+        //Member Methods
+
+    }
+}
